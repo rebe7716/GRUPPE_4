a98c98fdf9a5a49e0c5a1ab40fbfe409a2fc023c
rebe7716 <rebe7716@stud.kea.dk>
2021-09-10 14:27:18 +0200

    nyt javascript til burger

--- script.js
+++ script.js
@@ -62,16 +62,16 @@ function visFrugt() {
   });
 }
 
-//menubar, burgermenu
-function toggleMenu() {
-  console.log("toggleMenu");
-  document.querySelector("#menu").classList.toggle("hidden");
+// //menubar, burgermenu
+// function toggleMenu() {
+//   console.log("toggleMenu");
+//   document.querySelector("#menu").classList.toggle("hidden");
 
-  let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+//   let erSkjult = document.querySelector("#menu").classList.contains("hidden");
 
-  if (erSkjult == true) {
-    document.querySelector("#menuknap").textContent = "☰";
-  } else {
-    document.querySelector("#menuknap").textContent = "X";
-  }
-}
+//   if (erSkjult == true) {
+//     document.querySelector("#menuknap").textContent = "☰";
+//   } else {
+//     document.querySelector("#menuknap").textContent = "X";
+//   }
+// }
