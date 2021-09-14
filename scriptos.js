b391c7f14a3a23c2395b9bf168bae55325644d1f
Thea Miang <thea5897@stud.kea.dk>
2021-09-14 12:34:51 +0200

    MOBILE PORTRAET

--- scriptos.js
+++ scriptos.js
@@ -1,12 +1,12 @@
-//billeder portraet skift
-var w = window.innerWidth;
-console.log(w);
-if (w > 800) {
-  if (location.href == "thea.html") {
-    console.log(location);
-    document.querySelector(".portraet img").src = "pics/thea_desk.png";
-  }
-}
+// //billeder portraet skift
+// var w = window.innerWidth;
+// console.log(w);
+// if (w > 800) {
+//   if (location.href == "thea.html") {
+//     console.log(location);
+//     document.querySelector(".portraet img").src = "pics/thea_desk.png";
+//   }
+// }
 
 window.addEventListener("DOMContentLoaded", sidenVises);
 let frugter;
